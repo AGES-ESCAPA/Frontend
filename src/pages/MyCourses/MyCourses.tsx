4c362960e4545fd9d68a88fec293930783619aa4
Lucas Goettert Lopes <lucasgoettertlopes@gmail.com>
2026-09-21 14:45:52 -0300

    feat: adiciona estados e logica de filtro na tela MyCourses

--- src/pages/MyCourses/MyCourses.tsx
+++ src/pages/MyCourses/MyCourses.tsx
@@ -1,6 +1,26 @@
 import styles from './MyCourses.module.css';
+import { useState, useMemo } from 'react';
+import { mockCourses } from './mockCourses';
+
+export type TabType = 'Todos' | 'IN_PROGRESS' | 'PENDING' | 'COMPLETED';
 
 export const MyCourses = () => {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [searchTerm, setSearchTerm] = useState('');
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [activeTab, setActiveTab] = useState<TabType>('Todos');
+
+  const filteredCourses = useMemo(() => {
+    return mockCourses.filter((course) => {
+      const matchesTab = activeTab === 'Todos' || course.status === activeTab;
+
+      const matchesSearch =
+        course.title.toLowerCase().includes(searchTerm.toLowerCase()) ||
+        course.description.toLowerCase().includes(searchTerm.toLowerCase());
+      return matchesTab && matchesSearch;
+    });
+  }, [activeTab, searchTerm]);
+
   return (
     <div className={styles.container}>
       <aside className={styles.mockSidebar}>
@@ -15,6 +35,7 @@ export const MyCourses = () => {
         <section className={styles.pageContent}>
           <h1>My Courses</h1>
           <p>Continue where you left off</p>
+          <p>Cursos encontrados: {filteredCourses.length}</p>
         </section>
       </main>
     </div>
